94acdf6f80dad0556151360dab9d44887fc1b8ec
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2023-07-17 21:26:32 +0200

    connect db

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/dyner";
+  process.env.MONGODB_URI || "mongodb+srv://jessicamariafreund:ZqY3nWVqjUwyt33E@cluster0.5cx4k35.mongodb.net/dyner-server?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI)
